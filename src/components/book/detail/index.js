77076e2aa9b3dbe73c8977c856480010e39df440
haitv <haitv@sunshinegroup.vn>
2020-05-26 17:24:13 +0700

    Add some function in book

--- src/components/book/detail/index.js
+++ src/components/book/detail/index.js
@@ -1,32 +1,89 @@
 import * as React from 'react';
-import { View, Text, Image, StyleSheet, ScrollView, Dimensions } from 'react-native';
+import { View, Text, Image, StyleSheet, ScrollView, Dimensions, TouchableOpacity, Modal, TextInput, TouchableHighlight, Alert } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
 
 const { width } = Dimensions.get('window');
 
 export default function DetailBook({ route }) {
     const { data } = route.params;
-    return(
+    const { book, handleChangeFarvorite, createCommentNote } = data;
+    const [state, setState] = React.useState(book.is_farvorite);
+    const [visible, setVisible] = React.useState(false);
+    const [comment, setComment] = React.useState('');
+    return (
         <ScrollView showsVerticalScrollIndicator={false}>
             <View style={styles.container}>
                 <View style={styles.header}>
-                    <Image resizeMode="contain" source={{uri: data.image}} style={styles.image}/>
+                    <Image resizeMode="contain" source={{ uri: book.image }} style={styles.image} />
                 </View>
                 <View style={styles.label}>
                     {
-                        data.is_farvorite ? <FontAwesome style={styles.labelItem} name='heart' size={15} color='rgb(214, 49, 19)' />: 
-                        <FontAwesome style={styles.labelItem} name='heart' size={15} color='rgb(168, 165, 165)'/>
+                        state ? <TouchableOpacity style={[styles.button]} onPress={() => {
+                            setState(false)
+                            handleChangeFarvorite(book.id, false)
+                        }}>
+                            <View >
+                                <FontAwesome style={{ textAlign: 'left' }} name='heart' size={15} color='rgb(214, 49, 19)' />
+                            </View>
+                        </TouchableOpacity> :
+                            <TouchableOpacity style={[styles.button]} onPress={() => {
+                                setState(true)
+                                handleChangeFarvorite(book.id, true)
+                            }}>
+                                <View >
+                                    <FontAwesome style={{ textAlign: 'left' }} name='heart' size={15} color='rgb(168, 165, 165)' />
+                                </View>
+                            </TouchableOpacity>
+
                     }
-                    <Text style={[styles.text, styles.labelItem, {textAlign: 'right'}]}>{data.num_of_page_read}/{data.total_page}</Text>
+                    <Text style={[styles.text, styles.labelItem, { textAlign: 'right' }]}>{book.num_of_page_read}/{book.total_page}</Text>
+                    <View style={styles.labelItemRight}>
+                        <TouchableOpacity onPress={() => setVisible(true)}>
+                            <Text style={[styles.text, { textAlign: 'right' }]}>
+                                <FontAwesome name="comment" size={15} color="#fff"/>
+                             </Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
                 <View style={styles.commentContainer}>
                     {
-                        data.note.map((note, id) => <View key={id} style={styles.commentContent}>
+                        book.note.map((note, id) => <View key={id} style={styles.commentContent}>
                             <Text style={styles.text}>{note}</Text>
                         </View>)
                     }
                 </View>
             </View>
+            <Modal
+                visible={visible}
+                animationType="slide"
+                transparent={true}
+                onRequestClose={() => {}}
+            >
+                <View style={styles.centerView}>
+                    <View style={styles.modalView}>
+                        <TextInput 
+                        selectTextOnFocus={true} 
+                        autoFocus={true}
+                        multiline={true}
+                        style={styles.textInput} 
+                        onChangeText={comment => setComment(comment)}
+                        value={comment}
+                        />
+                        <View style={{ flexDirection: 'row' }}>
+                            <TouchableHighlight style={comment !== '' ? styles.buttonModal: {...styles.buttonModal, opacity: .8}} onPress={() => {
+                                createCommentNote(book.id, book.note, comment);
+                                book.note = [...book.note, comment];
+                                setVisible(false);
+                            }} disabled={comment === '' ? true: false}>
+                                <Text style={styles.buttonText}>Create</Text>
+                            </TouchableHighlight>
+                            <TouchableHighlight style={[styles.buttonClose , { marginLeft: 5 }]} onPress={() => setVisible(false)}>
+                                <Text style={styles.buttonText}>Close</Text>
+                            </TouchableHighlight>
+                        </View>
+                    </View>
+                </View>
+            </Modal>
         </ScrollView>
     )
 }
@@ -57,6 +114,9 @@ const styles = StyleSheet.create({
     labelItem: {
         width: '50%'
     },
+    labelItemRight: {
+        width: '45%'
+    },
     commentContainer: {
         flexDirection: 'column',
         width: width,
@@ -74,5 +134,47 @@ const styles = StyleSheet.create({
     text: {
         color: '#c4c0c0',
         fontSize: 14
+    },
+    button: {
+        alignItems: 'center'
+    },
+    centerView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 15,
+    },
+    modalView: {
+        backgroundColor: '#fff',
+        borderRadius: 20,
+        alignItems: 'center',
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: .25,
+        shadowRadius: 3.5,
+        elevation: 5,
+        width: Dimensions.get('window').width
+    },
+    textInput: {
+        height: 50,
+        width: width - 10,
+        borderColor: 'rgb(199, 121, 105)'
+    },
+    buttonModal: {
+        borderRadius: 30,
+        backgroundColor: '#f2400f',
+        width: 60
+    },
+    buttonClose: {
+        borderRadius: 30,
+        backgroundColor: '#124dcc',
+        width: 60 
+    },
+    buttonText: {
+        color: '#c4c0c0',
+        fontSize: 15,
+        textAlign: 'center' 
     }
 })
--- src/components/book/index.js
+++ src/components/book/index.js
@@ -1,23 +1,69 @@
 import * as React from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
+import { View, Text, Image, StyleSheet, TouchableOpacity, ScrollView, Modal, TextInput, CheckBox, Dimensions, TouchableHighlight } from 'react-native';
 import firebase, { getAppName, DB } from '../../firebase';
+import { FontAwesome } from '@expo/vector-icons';
 
 console.ignoredYellowBox = ['Setting a timer'];
+
+const { width, height } = Dimensions.get('window');
 
 class Book extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            books: []
+            books: [],
+            visible: false,
+            update: false,
+            model: {
+                name: '',
+                image: '',
+                category: 0,
+                is_farvorite: false,
+                note: [],
+                num_of_page_read: 0,
+                total_page: 0
+            }
         }
+
+        this.firebaseRef = firebase.firestore(getAppName()).collection(DB.book);
     }
 
     copyDocument() {
-        firebase.firestore(getAppName()).collection(DB.book).get()
+        this.firebaseRef.get()
             .then(result => result.docs.forEach(doc => {
                 firebase.firestore(getAppName()).collection(DB.book).add(doc.data())
             }))
+    }
+
+    updateDocument() {
+
+    }
+
+    getBooks() {
+        this.firebaseRef
+            .get()
+            .then(result => {
+                if (result.docs.length > 0) {
+                    let books = [];
+                    result.docs.forEach(doc => books.push({ id: doc.id, ...doc.data() }));
+                    this.setState({ books })
+                }
+            })
+    }
+
+    createCommentNote(docId, comments, comment) {
+        this.firebaseRef.doc(docId).update({
+            note: [...comments, comment]
+        }).then()
+            .catch(err => console.log(err))
+    }
+
+    handleChangeFarvorite(docId, state) {
+        this.firebaseRef.doc(docId).update({
+            is_farvorite: state
+        }).then()
+            .catch(err => console.log(err))
     }
 
     handleViewDetail(router, options) {
@@ -26,42 +72,166 @@ class Book extends React.Component {
         })
     }
 
+    handleChangeText(text, modelField) {
+        if (modelField === 'name')
+            this.setState(state => ({ model: { ...state.model, name: text } }));
+        else if (modelField === 'image')
+            this.setState(state => ({ model: { ...state.model, image: text } }));
+        else if (modelField === 'is_farvorite')
+            this.setState(state => ({ model: { ...state.model, is_farvorite: text } }));
+        else if (modelField === 'num_of_page_read')
+            this.setState(state => ({ model: { ...state.model, num_of_page_read: text } }));
+        else if (modelField === 'total_page')
+            this.setState(state => ({ model: { ...state.model, total_page: text } }));
+        else if (modelField === 'category')
+            this.setState(state => ({ model: { ...state.model, category: text } }));
+        else if (modelField === 'note') {
+            let note = [];
+            note.push(text);
+            this.setState(state => ({ model: { ...state.model, note: note } }));
+        }
+    }
+
+    createBook() {
+        if (this.state.model.name !== '' && this.state.model.image !== '') {
+            this.setState({ update: true, visible: false })
+            this.firebaseRef.add(this.state.model)
+                .then()
+                .catch(err => console.log(err))
+        }
+    }
+
     componentDidMount() {
-        firebase.firestore(getAppName()).collection(DB.book)
-            .get()
-            .then(result => {
-                if (result.docs.length > 0) {
-                    let books = [];
-                    result.docs.forEach(doc => books.push(doc.data()));
-                    this.setState({ books })
-                }
-            })
-        //this.copyDocument()
+        this.getBooks()
+    }
+
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     if(nextState !== this.state) {
+    //         return true
+    //     }
+    //     return false;
+    // }
+
+    componentDidUpdate(prevProps, prevState) {
+        if (this.state.update) {
+            this.getBooks();
+            this.setState({ update: false })
+        }
     }
 
     render() {
         return (
-            <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{marginTop: 10}}>
-                <View style={styles.container}>
-                    {
-                        this.state.books.length > 0 ? this.state.books.map((book, id) => <View style={[styles.bookContainer, { position: 'relative' }]} key={id} >
-                            <View style={styles.label}>
-                                <Text style={styles.labelText}>{book.num_of_page_read}/{book.total_page}</Text>
-                            </View>
-                            <Image source={{ uri: book.image }} style={[styles.image, { opacity: .5 }]}>
-                            </Image>
-                            <TouchableOpacity onPress={() => this.handleViewDetail('Detail Book', book)}>
-                                <Text style={[styles.text]}>{book.name}</Text>
-                            </TouchableOpacity>
-                        </View>) : null
-                    }
+            <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={{ marginTop: 10, flexGrow: 1 }}>
+                <View style={styles.mainContainer}>
+                    <View style={styles.container}>
+                        {
+                            this.state.books.length > 0 ? this.state.books.map((book, id) => <View style={[styles.bookContainer, { position: 'relative' }]} key={id} >
+                                <View style={styles.label}>
+                                    <Text style={styles.labelText}>{book.num_of_page_read}/{book.total_page}</Text>
+                                </View>
+                                <Image source={{ uri: book.image }} style={[styles.image, { opacity: .5 }]}>
+                                </Image>
+                                <TouchableOpacity onPress={() => this.handleViewDetail('Detail Book', {
+                                    book: book,
+                                    handleChangeFarvorite: this.handleChangeFarvorite.bind(this),
+                                    createCommentNote: this.createCommentNote.bind(this)
+                                })}>
+                                    <Text style={[styles.text]}>{book.name}</Text>
+                                </TouchableOpacity>
+                            </View>) : null
+                        }
+                    </View>
+                    <TouchableOpacity style={[styles.buttonBottom]} onPress={() => this.setState({ visible: true, model: {
+                        name: '',
+                        image: '',
+                        category: 0,
+                        is_farvorite: false,
+                        note: [],
+                        num_of_page_read: 0,
+                        total_page: 0
+                    } })}>
+                        <FontAwesome name='plus-circle' size={25} color='#cc3112' />
+                    </TouchableOpacity>
                 </View>
+                <Modal
+                    visible={this.state.visible}
+                    animationType="slide"
+                    transparent={true}
+                    onRequestClose={() => { }}>
+                    <View style={styles.centerView}>
+                        <View style={styles.modalView}>
+                            <Text style={styles.marginLeft}>Name</Text>
+                            <TextInput
+                                style={[styles.textInput, styles.marginLeft]}
+                                selectTextOnFocus={true}
+                                multiline={true}
+                                onChangeText={text => this.handleChangeText(text, 'name')}
+                                value={this.state.model.name} />
+                            <Text style={styles.marginLeft}>Farvorite</Text>
+                            <CheckBox
+                                style={{ marginLeft: 5 }}
+                                onValueChange={value => this.handleChangeText(value, 'is_farvorite')}
+                                value={this.state.model.is_farvorite} />
+                            <Text style={styles.marginLeft}>Image</Text>
+                            <TextInput
+                                style={[styles.textInput, styles.marginLeft]}
+                                selectTextOnFocus={true}
+                                multiline={true}
+                                onChangeText={text => this.handleChangeText(text, 'image')}
+                                value={this.state.model.image} />
+                            <Text style={styles.marginLeft}>Category</Text>
+                            <TextInput
+                                style={[styles.textInput, styles.marginLeft]}
+                                selectTextOnFocus={true}
+                                multiline={true}
+                                onChangeText={text => this.handleChangeText(parseInt(text), 'category')}
+                                value={this.state.model.category} />
+                            <Text style={styles.marginLeft}>Note</Text>
+                            <TextInput
+                                style={[styles.textInput, styles.marginLeft]}
+                                selectTextOnFocus={true}
+                                multiline={true}
+                                onChangeText={text => this.handleChangeText(text, 'note')}
+                            />
+                            <Text style={styles.marginLeft}>Num of page read</Text>
+                            <TextInput
+                                style={[styles.textInput, styles.marginLeft]}
+                                selectTextOnFocus={true}
+                                multiline={true}
+                                onChangeText={text => this.handleChangeText(parseInt(text), 'num_of_page_read')}
+                                value={this.state.model.num_of_page_read}
+                            />
+                            <Text style={styles.marginLeft}>Total page</Text>
+                            <TextInput
+                                style={[styles.textInput, styles.marginLeft]}
+                                selectTextOnFocus={true}
+                                multiline={true}
+                                onChangeText={text => this.handleChangeText(parseInt(text), 'total_page')}
+                                value={this.state.model.total_page}
+                            />
+                            <View style={styles.buttonGroup}>
+                                <TouchableHighlight
+                                    style={(this.state.model.name === '' || this.state.model.image === '') ? { ...styles.buttonSave, opacity: .8 } : styles.buttonSave}
+                                    onPress={() => this.createBook()} disabled={(this.state.model.name === '' || this.state.model.image === '')? true: false}>
+                                    <Text style={[styles.text, styles.buttonText]}>Save</Text>
+                                </TouchableHighlight>
+                                <TouchableHighlight style={styles.buttonClose} onPress={() => this.setState({ visible: false })}>
+                                    <Text style={[styles.text, styles.buttonText]}>Close</Text>
+                                </TouchableHighlight>
+                            </View>
+                        </View>
+                    </View>
+                </Modal>
             </ScrollView>
         )
     }
 }
 
 const styles = StyleSheet.create({
+    mainContainer: {
+        flex: 1,
+        flexDirection: 'column'
+    },
     container: {
         flex: 1,
         flexDirection: 'row',
@@ -95,6 +265,76 @@ const styles = StyleSheet.create({
     text: {
         color: '#c4c0c0',
         fontSize: 14
+    },
+    bottom: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        marginBottom: 10
+    },
+    buttonBottom: {
+        position: 'absolute',
+        bottom: 20,
+        right: 35,
+        zIndex: 2
+    },
+    centerView: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 15,
+    },
+    modalView: {
+        backgroundColor: '#fff',
+        borderRadius: 20,
+        alignItems: 'flex-start',
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: .25,
+        shadowRadius: 3.5,
+        elevation: 5,
+        width: width,
+    },
+    buttonModal: {
+        borderRadius: 30,
+        backgroundColor: '#f2400f',
+        width: 60
+    },
+    buttonClose: {
+        borderRadius: 30,
+        backgroundColor: '#124dcc',
+        width: 60
+    },
+    buttonText: {
+        color: '#c4c0c0',
+        fontSize: 15,
+        textAlign: 'center'
+    },
+    textInput: {
+        borderBottomWidth: 2,
+        borderBottomColor: 'red',
+        width: width - 15
+    },
+    marginLeft: {
+        marginLeft: 10
+    },
+    buttonGroup: {
+        flexDirection: 'row',
+        width: width,
+        marginVertical: 10
+    },
+    buttonSave: {
+        width: 50,
+        marginLeft: 10,
+        backgroundColor: '#f2400f'
+    },
+    buttonClose: {
+        width: 50,
+        marginLeft: 10
+    },
+    buttonText: {
+        textAlign: 'center'
     }
 });
 
